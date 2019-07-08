1e7ad78208fc9b8aba9f5f93ad3e012a739f0e16
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-08 18:49:07 +0200

    ok

--- random/main.js
+++ random/main.js
@@ -1,40 +1,31 @@
 
 
-
 function casuale(){
   var risultato = Math.floor(Math.random(1) * (10+1));
   // console.log('questo e il risultato random',risultato);
   return risultato;
 }
 
-//numUtente da il valore a l argomento numero
-// richiamo la funzione e inserisco il nome dell argomento
-
+var utente = ("numero utente",casuale());
+console.log("numero utente",utente);
 
 var pc = (casuale());
 console.log("numero pc",pc);
-var utente = ("numero utente",casuale());
-console.log("numero utente",utente);
+
 var estratto = ("numero estratto",casuale());
+console.log("il numero estrato =",estratto);
 
 if (utente == estratto) {
-  console.log("hai vinto");
+  console.log(utente,"hai vinto");
 }
 else if (pc == estratto) {
-  console.log(tente);
-  console.log("ha vinto pc");
+  console.log(pc,"ha vinto pc");
+}
+else if(pc == estratto && utente == estratto){
+  console.log("avete vintoooo entrambi");
 }
 else {
   console.log("avete perso entrambi");
-}
-
-if(utente % 2 == 0){
-  console.log(utente);
-  console.log("pari");
-}
-else {
-  console.log(utente);
-  console.log("dispari");
 }
 
 
